13905cc59be73dec9e7031c14286cf231219be7c
tusmit.shah <tusmit.shah@1click.tech>
2025-07-08 14:41:45 +0530

    udpte code

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // base: '/VdtdpAttendance/', // 👈 use your repo name here
+    base: '/VdtdpAttendance/', // ✅ very important for GitHub Pages
+
   plugins: [react()],
    build: {
         chunkSizeWarningLimit: 2000 // or higher
